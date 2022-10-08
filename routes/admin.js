4266407ef32c53c5ad1cae5ba49f26d91cc02c0e
gokul raj <gokulrajan007tvm@gmail.com>
2022-10-08 10:19:30 +0530

    40% completed

--- routes/admin.js
+++ routes/admin.js
@@ -1,9 +1,134 @@
 var express = require('express');
 var router = express.Router();
+const multer = require('multer');
+var db=require('../config/connection');
+const adminhelper = require('../helpers/adminhelper');
+const uuid=require('uuid').v4
+var objectid=require('mongodb').ObjectId
+
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  adminhelper.getall(db.get().collection('category').find()).then((category)=>{
+    
+    res.render('admin/addproducts',{admin:true,category});;
+    
+  })
+ 
 });
+router.get('/addcategories', function(req, res, next) {
+res.render('admin/addcategories',{admin:true});
+});
+
+const storage =multer.diskStorage({
+  destination:(req,file,cb)=>{
+    const path = require('path')
+    cb(null,path.join(__dirname,'../public/product-images'))
+
+  },
+  filename:(req,file,cb)=>{
+    const{originalname}=file;
+    var imgid=`${uuid()}.jpg`
+    cb(null,imgid);
+  }
+}); 
+const upload=multer({storage:storage})
+router.post('/addcategories',upload.single('category'), function(req, res) {
+  let categoryobj={
+    name:req.body.name,
+    imagename:req.file.filename
+
+}
+
+  adminhelper.addtodatabase(db.get().collection('category').insertOne(categoryobj)).then((data)=>{
+    
+    res.redirect('admin/addcategories',{admin:true});
+   
+   
+  })
+  
+  
+});
+
+router.post('/addproducts',upload.single('image'), function(req, res) {
+  let productobj={
+    name:req.body.name,
+    category:req.body.category,
+    price:req.body.price,
+    description:req.body.description,
+    expdate:req.body.expirydate,
+    quantity:req.body.quantity,
+    totalnumber:req.body.totalnumber,
+    star:req.body.star,
+    imagename:req.file.filename
+}
+
+  adminhelper.addtodatabase(db.get().collection('products').insertOne(productobj)).then((data)=>{
+    res.redirect('/admin');
+    
+  })
+  
+  
+})
+router.get('/viewproducts', function(req, res, next) {
+  adminhelper.getall(db.get().collection('products').find()).then((products)=>{
+    res.render('admin/viewproducts',{admin:true,products});
+  })
+   
+});
+router.get('/editproducts', function(req, res, next) {
+  adminhelper.getall(db.get().collection('products').find()).then((products)=>{
+    res.render('admin/editproducts',{admin:true,products});
+  })
+   
+});
+
+router.get('/deleteproduct/:id',(req,res)=>{
+  let proid=req.params.id
+  adminhelper.deleteone(  db.get().collection('products').remove({_id:objectid(proid)})).then((response)=>{
+    res.redirect('/admin/viewproducts')
+  })
+})
+router.get('/editproduct/:id',async(req,res)=>{
+  let proid=req.params.id
+  let product = await adminhelper.getoneitemdetails( db.get().collection('products').findOne({_id:objectid(proid)}))
+  res.render('admin/editproductpage',{admin:true,product})
+})
+router.post('/editproducts/:id',upload.single('image'),function(req,res){
+  console.log(req.body)
+  let proid=req.params.id
+  const name = req.body.name;
+  const category = req.body.category;
+  const price = req.body.price;
+  const description=req.body.description;
+  const expdate=req.body.expdate;
+  const quantity=req.body.quantity;
+  const totalnumber=req.body.totalnumber;
+
+
+  const updates = {
+    name,
+    category,
+    price,
+    description,
+    expdate,
+    quantity,
+    totalnumber,
+};
+
+if (req.file) {
+  const image = req.file.filename;
+  updates.imagename = image;
+}
+adminhelper.updateone(db.get().collection('products')
+.updateOne({_id:objectid(proid)},{
+    $set:updates
+})).then(()=>{
+  res.redirect('/admin/editproducts')
+})
+
+})
+
+
 
 module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,40 @@
 var express = require('express');
 var router = express.Router();
+const multer = require('multer');
+var db=require('../config/connection');
+const adminhelper = require('../helpers/adminhelper');
+const uuid=require('uuid').v4
+var objectid=require('mongodb').ObjectId
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  adminhelper.getall(db.get().collection('category').find()).then((category)=>{
+    adminhelper.getall(db.get().collection('products').find({'category':'Bamboo Products'})).then((bambooproducts)=>{
+      res.render('user/home',{user:true,category,bambooproducts});
+    })
+  })
+ 
+});
+router.get('/login', function(req, res, next) {
+  res.render('user/login',{user:true});
+});
+router.get('/signup', function(req, res, next) {
+  res.render('user/signup',{user:true});
+});
+router.get('/catdata/:name',(req,res)=>{
+  let name =req.params.name
+  console.log(name)
+  adminhelper.getall(db.get().collection('products').find({'category': name})).then((response)=>{ 
+   res.json(response)
+    
+ })
+
+})
+router.get('/showproduct/:id', async function(req, res, next) {
+  let proid =req.params.id
+  let product = await adminhelper.getoneitemdetails( db.get().collection('products').findOne({_id:objectid(proid)}))
+  console.log(product)
+  res.render('user/showproduct',{user:true,product});
 });
 
 module.exports = router;
